851b7eba9a248695c1c0d21c178833435911ab2d
evertonrafaelumbelino <evertonrafaelumbelino@gmail.com>
2025-04-28 13:04:13 -0300

    Arquivos da Landing Page

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+let imgMenu = document.getElementById('imgMenu');
+let menu = document.getElementById('menu');
+let inputNome = document.getElementById('nome');
+let inputEmail = document.getElementById('email');
+let textAreaMensagem = document.getElementById('mensagem');
+let inputEnviar = document.getElementById('inputEnviar');
+let textoFormResultado = document.getElementById('textoFormResultado');
+
+imgMenu.addEventListener('click', () => {
+    if (imgMenu.className === 'close') {
+        menu.style.position = 'absolute';
+        menu.style.display = 'flex';
+        menu.style.flexFlow = 'column nowrap';
+        menu.style.justifyContent = 'center';
+        menu.style.alignItems = 'center';
+        menu.style.top = '13%';
+        menu.style.left = '47vw';
+        menu.style.backgroundColor = '#618a45';
+        menu.style.padding = '10px';
+        menu.style.width = '50%';
+        imgMenu.className = 'open';
+    } else {
+        menu.style.display = 'none';
+        imgMenu.className = 'close';
+    }
+});
+
+inputEnviar.addEventListener('click', () => {
+    if (inputNome.value.length === 0 | inputEmail.value.length === 0 | textAreaMensagem.value.length === 0) {
+        textoFormResultado.innerHTML = 'Preencha todos os campos!';
+        textoFormResultado.style.color = '#7b1313';
+        textoFormResultado.style.fontWeight = 'bold';
+        textoFormResultado.style.textAlign = 'center';
+    } else {
+        textoFormResultado.innerHTML = 'Mensagem enviada com sucesso!';
+        textoFormResultado.style.color = '#12ff46';
+        textoFormResultado.style.fontWeight = 'bold';
+        textoFormResultado.style.textAlign = 'center';
+    }
+});
